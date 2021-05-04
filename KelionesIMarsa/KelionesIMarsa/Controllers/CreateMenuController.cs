2ffad3ba82349a56aa353ad37bb74cd2cbd13e28
DovVai <73988159+DovVai@users.noreply.github.com>
2021-05-04 11:21:00 +0300

    Pridėti viewsai ir controlleriai kelionės registracijai

---
+++ KelionesIMarsa/KelionesIMarsa/Controllers/CreateMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KelionesIMarsa.Controllers
+{
+    public class CreateMenuController : Controller
+    {
+        // GET: CreateMenu
+        public ActionResult CreateMenu()
+        {
+            return View();
+        }
+    }
+}
