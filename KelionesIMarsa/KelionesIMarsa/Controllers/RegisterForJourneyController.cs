a99f13736dd69190679748694217111ca895366e
DovVai <73988159+DovVai@users.noreply.github.com>
2021-05-04 11:34:46 +0300

    .

---
+++ KelionesIMarsa/KelionesIMarsa/Controllers/RegisterForJourneyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KelionesIMarsa.Controllers
+{
+    public class RegisterForJourneyController : Controller
+    {
+        public ActionResult Journey()
+        {
+            return View();
+        }
+        public ActionResult RegisterForJourney()
+        {
+            return View();
+        }
+        public ActionResult TimeTableCreate()
+        {
+            return View();
+        }
+    }
+}
