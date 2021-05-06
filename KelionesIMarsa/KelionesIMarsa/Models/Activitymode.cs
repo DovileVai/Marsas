6f95505b3523dedf1524c75c9d63b87392a07090
DovVai <73988159+DovVai@users.noreply.github.com>
2021-05-06 18:09:58 +0300

    prideti modeliai

---
+++ KelionesIMarsa/KelionesIMarsa/Models/Activitymode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KelionesIMarsa.Models
+{
+    public class Activitymode
+    {
+        [DisplayName("Pradžia")]
+        public int startTime { get; set; }
+
+        [DisplayName("ID")]
+        public int id_activityMode { get; set; }
+        [DisplayName("Veiklos ID")]
+        public int fk_Activityid_Activity { get; set; }
+
+        [DisplayName("Tvarkaraščio ID")]
+        public int fk_ActivitiesScheduleid_ActivitiesSchedule { get; set; }
+
+
+
+
+    }
+}
---
+++ KelionesIMarsa/KelionesIMarsa/Models/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace KelionesIMarsa.Models
+{
+    public class Menu
+    {
+        [DisplayName("Savaitės diena")]
+        public string weekDay { get; set; }
+
+        [DisplayName("Pradžios laikas")]
+        public int startTime { get; set; }
+        [DisplayName("ID")]
+        public int id_Menu { get; set; }
+
+        [DisplayName("Patiekalo id")]
+        public int fk_Mealid_Meal { get; set; }
+
+        [DisplayName("Užsakymo ID")]
+        public int fk_OrderAid_OrderA{ get; set; }
+
+    }
+}
