34cebd277b93ce01ddbfa042194020c1c3848b6b
DovVai <73988159+DovVai@users.noreply.github.com>
2021-05-06 17:14:22 +0300

    prideta registracija kelionei

---
+++ KelionesIMarsa/KelionesIMarsa/Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KelionesIMarsa.Models
+{
+    public class Order
+    {
+        [DisplayName("Užsakymo data")]
+        public DateTime dateOfOrder { get; set; }
+
+        [DisplayName("Asmens kodas")]
+        public string personalCode { get; set; }
+
+        [DisplayName("Telefonas")]
+        public string phone { get; set; }
+
+        [DisplayName("AR sumokėta")]
+        public bool payment { get; set; }
+
+        [DisplayName("ID")]
+        [Required]
+        public int id_OrderA { get; set; }
+
+        [DisplayName("keliones ID")]
+        public int fk_Journeyid_Journey { get; set; }
+ 
+    }
+}
