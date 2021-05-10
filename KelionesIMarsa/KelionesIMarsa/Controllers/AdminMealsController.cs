d9f9520e4f9bc216ae301bf1d49ff58bea5defc6
DovVai <73988159+DovVai@users.noreply.github.com>
2021-05-10 15:32:23 +0300

    veiksmai su db perkelti i modelius

--- KelionesIMarsa/KelionesIMarsa/Controllers/AdminMealsController.cs
+++ KelionesIMarsa/KelionesIMarsa/Controllers/AdminMealsController.cs
@@ -15,28 +15,8 @@ namespace KelionesIMarsa.Controllers
         // GET: AdminMeals
         public ActionResult MealList()
         {
-            List<Meal> meals = new List<Meal>();
-            string connectionstring = "datasource=localhost;port=3306;username=root;password=";
-            string mysql = "SELECT * From marsodb.meal";
-            MySqlConnection conn = new MySqlConnection(connectionstring);
-            MySqlCommand command = new MySqlCommand(mysql, conn);
-            conn.Open();
-            MySqlDataAdapter dtb = new MySqlDataAdapter();
-            dtb.SelectCommand = command;
-            DataTable dt = new DataTable();
-            dtb.Fill(dt);
-            conn.Close();
-            int a = 0;
-            foreach (DataRow item in dt.Rows)
-            {
-                meals.Add(new Meal
-                {
-                    name = Convert.ToString(item["name"]),
-                    calories = Convert.ToInt32(item["calories"]),
-                    type = Convert.ToString(item["type"]),
-                    id_Meal = Convert.ToInt32(item["id_Meal"])
-                });
-            }
+            Meal meal = new Meal();
+            List<Meal> meals = meal.MealList();
             return View(meals.ToList());
         }
 
@@ -53,19 +33,7 @@ namespace KelionesIMarsa.Controllers
         {
             try
             {
-                Random rnd = new Random();
-                int id = rnd.Next(0, 99999);
-                string conn = "datasource=localhost;port=3306;username=root;password=";
-                MySqlConnection mySqlConnection = new MySqlConnection(conn);
-                string sqlquery = @"INSERT INTO marsodb.meal(name,calories,type,id_Meal)VALUES(?n,?c,?t,?id);";
-                MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
-                mySqlCommand.Parameters.Add("?n", MySqlDbType.VarChar).Value = newMeal.name;
-                mySqlCommand.Parameters.Add("?c", MySqlDbType.Int32).Value = newMeal.calories;
-                mySqlCommand.Parameters.Add("?t", MySqlDbType.VarChar).Value = newMeal.type;
-                mySqlCommand.Parameters.Add("?id", MySqlDbType.VarChar).Value = id;
-                mySqlConnection.Open();
-                mySqlCommand.ExecuteNonQuery();
-                mySqlConnection.Close();
+                newMeal.CreateMeal(newMeal);
 
                 return RedirectToAction("MealList");
             }
@@ -74,45 +42,12 @@ namespace KelionesIMarsa.Controllers
                 return View(newMeal);
             }
         }
-        public Meal getMeal(int id)
-        {
-            Meal meal = new Meal();
-            string conn = "datasource=localhost;port=3306;username=root;password=";
-            MySqlConnection mySqlConnection = new MySqlConnection(conn);
-            string sqlquery = "select * from marsodb.meal where id_Meal=?id";
-            MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
-            mySqlCommand.Parameters.Add("?id", MySqlDbType.Int32).Value = id;
-            mySqlConnection.Open();
-            MySqlDataAdapter mda = new MySqlDataAdapter(mySqlCommand);
-            DataTable dt = new DataTable();
-            mda.Fill(dt);
-            mySqlConnection.Close();
-
-            foreach (DataRow item in dt.Rows)
-            {
-                meal.id_Meal = Convert.ToInt32(item["id_Meal"]);
-                meal.type = Convert.ToString(item["type"]);
-                meal.calories = Convert.ToInt32(item["calories"]);
-                meal.name = Convert.ToString(item["name"]);
-            }
-
-            return meal;
-        }
+       
         public bool updateMeal(Meal meal)
         {
             try
             {
-                string conn = "datasource=localhost;port=3306;username=root;password=";
-                MySqlConnection mySqlConnection = new MySqlConnection(conn);
-                string sqlquery = @"UPDATE marsodb.meal a SET a.type=?t, a.calories=?c,a.name=?n WHERE a.id_Meal=?id";
-                MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
-                mySqlCommand.Parameters.Add("?t", MySqlDbType.VarChar).Value = meal.type;
-                mySqlCommand.Parameters.Add("?c", MySqlDbType.VarChar).Value = meal.calories;
-                mySqlCommand.Parameters.Add("?n", MySqlDbType.VarChar).Value = meal.name;
-                mySqlCommand.Parameters.Add("?id", MySqlDbType.VarChar).Value = meal.id_Meal;
-                mySqlConnection.Open();
-                mySqlCommand.ExecuteNonQuery();
-                mySqlConnection.Close();
+                meal.updateMeal(meal);
                 return true;
             }
             catch (Exception)
@@ -120,21 +55,12 @@ namespace KelionesIMarsa.Controllers
                 return false;
             }
         }
-        public void deleteM(int id)
-        {
-            string conn = "datasource=localhost;port=3306;username=root;password=";
-            MySqlConnection mySqlConnection = new MySqlConnection(conn);
-            string sqlquery = @"DELETE FROM marsodb.meal where id_Meal=?id";
-            MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
-            mySqlCommand.Parameters.Add("?id", MySqlDbType.VarChar).Value = id;
-            mySqlConnection.Open();
-            mySqlCommand.ExecuteNonQuery();
-            mySqlConnection.Close();
-        }
+        
         // GET: AdminMeals/Edit/5
         public ActionResult EditMeal(int id)
         {
-            return View(getMeal(id));
+            Meal meal = new Meal();
+            return View(meal.getMeal(id));
         }
 
         // POST: AdminMeals/Edit/5
@@ -159,7 +85,8 @@ namespace KelionesIMarsa.Controllers
         // GET: AdminMeals/Delete/5
         public ActionResult DeleteMeal(int id)
         {
-            return View(getMeal(id));
+            Meal meal = new Meal();
+            return View(meal.getMeal(id));
         }
 
         // POST: AdminMeals/Delete/5
@@ -168,7 +95,8 @@ namespace KelionesIMarsa.Controllers
         {
             try
             {
-                deleteM(id);
+                Meal meal = new Meal();
+                meal.deleteM(id);
                 return RedirectToAction("MealList");
             }
             catch
--- KelionesIMarsa/KelionesIMarsa/Models/Activity.cs
+++ KelionesIMarsa/KelionesIMarsa/Models/Activity.cs
@@ -4,6 +4,11 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Web.Mvc;
+using KelionesIMarsa.Models;
+using MySql.Data.MySqlClient;
 
 namespace KelionesIMarsa.Models
 {
@@ -20,8 +25,100 @@ namespace KelionesIMarsa.Models
 
         [DisplayName("ID")]
         public int id_Activity { get; set; }
-        
-        
+
+        public List<Activity> ActivitiesList()
+        {
+            List<Activity> activities = new List<Activity>();
+            string connectionstring = "datasource=localhost;port=3306;username=root;password=";
+            string mysql = "SELECT * From marsodb.activity";
+            MySqlConnection conn = new MySqlConnection(connectionstring);
+            MySqlCommand command = new MySqlCommand(mysql, conn);
+            conn.Open();
+            MySqlDataAdapter dtb = new MySqlDataAdapter();
+            dtb.SelectCommand = command;
+            DataTable dt = new DataTable();
+            dtb.Fill(dt);
+            conn.Close();
+            int a = 0;
+            foreach (DataRow item in dt.Rows)
+            {
+                activities.Add(new Activity
+                {
+                    type = Convert.ToString(item["type"]),
+                    duration = Convert.ToInt32(item["duration"]),
+                    name = Convert.ToString(item["name"]),
+                    id_Activity = Convert.ToInt32(item["id_Activity"])
+                });
+            }
+            return activities;
+        }
+        public void CreateActivity(Activity newActivity)
+        {
+                Random rnd = new Random();
+                int id = rnd.Next(0, 99999);
+                string conn = "datasource=localhost;port=3306;username=root;password=";
+                MySqlConnection mySqlConnection = new MySqlConnection(conn);
+                string sqlquery = @"INSERT INTO marsodb.activity(type,duration,name,id_Activity)VALUES(?t,?d,?n,?id);";
+                MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+                mySqlCommand.Parameters.Add("?t", MySqlDbType.VarChar).Value = newActivity.type;
+                mySqlCommand.Parameters.Add("?d", MySqlDbType.Int32).Value = newActivity.duration;
+                mySqlCommand.Parameters.Add("?n", MySqlDbType.VarChar).Value = newActivity.name;
+                mySqlCommand.Parameters.Add("?id", MySqlDbType.VarChar).Value = id;
+                mySqlConnection.Open();
+                mySqlCommand.ExecuteNonQuery();
+                mySqlConnection.Close();
+
+        }
+        public Activity getActivity(int id)
+        {
+            Activity activity = new Activity();
+            string conn = "datasource=localhost;port=3306;username=root;password=";
+            MySqlConnection mySqlConnection = new MySqlConnection(conn);
+            string sqlquery = "select * from marsodb.activity where id_Activity=?id";
+            MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+            mySqlCommand.Parameters.Add("?id", MySqlDbType.Int32).Value = id;
+            mySqlConnection.Open();
+            MySqlDataAdapter mda = new MySqlDataAdapter(mySqlCommand);
+            DataTable dt = new DataTable();
+            mda.Fill(dt);
+            mySqlConnection.Close();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                activity.id_Activity = Convert.ToInt32(item["id_Activity"]);
+                activity.type = Convert.ToString(item["type"]);
+                activity.duration = Convert.ToInt32(item["duration"]);
+                activity.name = Convert.ToString(item["name"]);
+            }
+
+            return activity;
+        }
+        public void updateActivity(Activity activity)
+        {
+                string conn = "datasource=localhost;port=3306;username=root;password=";
+                MySqlConnection mySqlConnection = new MySqlConnection(conn);
+                string sqlquery = @"UPDATE marsodb.activity a SET a.type=?t, a.duration=?d,a.name=?n WHERE a.id_Activity=?id";
+                MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+                mySqlCommand.Parameters.Add("?t", MySqlDbType.VarChar).Value = activity.type;
+                mySqlCommand.Parameters.Add("?d", MySqlDbType.VarChar).Value = activity.duration;
+                mySqlCommand.Parameters.Add("?n", MySqlDbType.VarChar).Value = activity.name;
+                mySqlCommand.Parameters.Add("?id", MySqlDbType.VarChar).Value = activity.id_Activity;
+                mySqlConnection.Open();
+                mySqlCommand.ExecuteNonQuery();
+                mySqlConnection.Close();
+
+        }
+        public void deleteA(int id)
+        {
+            string conn = "datasource=localhost;port=3306;username=root;password=";
+            MySqlConnection mySqlConnection = new MySqlConnection(conn);
+            string sqlquery = @"DELETE FROM marsodb.activity where id_Activity=?id";
+            MySqlCommand mySqlCommand = new MySqlCommand(sqlquery, mySqlConnection);
+            mySqlCommand.Parameters.Add("?id", MySqlDbType.VarChar).Value = id;
+            mySqlConnection.Open();
+            mySqlCommand.ExecuteNonQuery();
+            mySqlConnection.Close();
+        }
 
 
 
